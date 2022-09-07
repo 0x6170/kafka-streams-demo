87a516a3d456e6bc3715395cd56832de0e14e725
0x6170 <5549700+0x6170@users.noreply.github.com>
2022-09-07 12:46:07 +0200

    Add link to apache kafka documentation regarding some setting

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -15,7 +15,7 @@ public class App {
     public static void main(String... args) {
 
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "Weather filter");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "Weather filter"); // this equals to "group.id" in an ordinary kafka consumer https://kafka.apache.org/32/javadoc/org/apache/kafka/streams/StreamsConfig.html
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Integer().getClass());
